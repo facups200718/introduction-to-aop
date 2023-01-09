2a1620deb97d7ee51a05939d954af9473ba1088f
SYNAPSE\facundo.perez <facundo.perez@globallogic.com>
2023-01-09 20:05:31 -0300

    Aspects con anotaciones

--- aop/src/main/java/com/percipio/aop/aspects/Log.java
+++ aop/src/main/java/com/percipio/aop/aspects/Log.java
@@ -1,10 +1,22 @@
 package com.percipio.aop.aspects;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+//Indicamos que es un componente para que el escaneo detallado en el application context lo encuentre
+//y también indicamos que es un aspect
+@Component
+@Aspect
 public class Log {
+
+    //Indicamos a traves de las anotaciones que este metodo se va a ejecutar antes siempre que ejecutemos drive() de la clase Car
+    @Before("execution(void com.percipio.aop.vehicles.Car.drive())")
     public void vehicleAboutToMove() {
         System.out.println("Saving to log: Vehicle action: About to move");
     }
 
+    @Before("execution(void com.percipio.aop.vehicles.Car.stop())")
     public void vehicleAboutToStop() {
         System.out.println("Saving to log: Vehicle action: About to stop");
     }
