6d3e31975bf643a5d66c7763ac1a94953fe14b6b
SYNAPSE\facundo.perez <facundo.perez@globallogic.com>
2023-01-09 23:34:23 -0300

    Accediendo a la información del joinpoint en el advice

--- aop/src/main/java/com/percipio/aop/AopApplication.java
+++ aop/src/main/java/com/percipio/aop/AopApplication.java
@@ -10,7 +10,15 @@ public class AopApplication {
 	public static void main(String[] args) {
 		var applicationContext = new ClassPathXmlApplicationContext("appContext.xml");
 	    Car myCar = applicationContext.getBean("car", Car.class);
-		myCar.drive();
+		try {
+			myCar.drive(120);
+		} catch (Exception exception) {
+			System.out.println("Oops! No driving today.");
+			System.out.println(exception);
+		}
+
+		applicationContext.close();
+
 	}
 
 }
--- aop/src/main/java/com/percipio/aop/vehicles/Car.java
+++ aop/src/main/java/com/percipio/aop/vehicles/Car.java
@@ -1,12 +1,12 @@
 package com.percipio.aop.vehicles;
 
 public class Car {
-    public void drive() {
+    public void drive(int speed) {
         //meeting tracing concern
         //System.out.println("Starting to log (from Car class): Car is driving");
 
         //actual business logic for driving
-        System.out.println("Accelerating and moving");
+        System.out.println("I am driving at the speed of " + speed + " km/h");
     }
 
     public void stop() {
